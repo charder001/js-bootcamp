7070b125fb9b104c3b4e6feb7f3b1f779fa9705c
charder001 <43436118+charder001@users.noreply.github.com>
2019-04-22 12:19:09 +0200

    Update l1-fizzbuzz.js

--- l1-fizzbuzz.js
+++ l1-fizzbuzz.js
@@ -1,1 +1,14 @@
-r
+let hash = 0;
+
+for (i = 0; i < 100; i++) {
+  hash++;
+  if (hash % 3 === 0 && hash % 5 === 0) {
+    console.log("fizzbuzz")
+  } else if (hash % 3 === 0) {
+    console.log("fizz")
+  } else if (hash % 5 === 0) {
+    console.log("buzz")
+  } else {
+    console.log(hash);
+  }
+}
