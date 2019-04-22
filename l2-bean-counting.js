bbe1887c88cec9229931e4170ac54b8ef4b6b576
charder001 <43436118+charder001@users.noreply.github.com>
2019-04-22 14:36:40 +0200

    Update l2-bean-counting.js

--- l2-bean-counting.js
+++ l2-bean-counting.js
@@ -1,1 +1,13 @@
- 
+ var charCount = 0
+// Your code here.
+function countChar(value, char) {
+  for (i = 0; i < value.length; i++) {
+    if (value[i] === char) {
+     charCount++
+    }
+  }
+  return charCount
+}
+
+console.log(countChar("kakkerlak", "k"));
+// → 4
